dd485c3c886265e9cb9a09b6c3a656b10a943c18
bala-amypo <bala@amypo.in>
2025-12-18 05:27:58 +0000

    User commit

--- src/main/entity/User.java
+++ src/main/entity/User.java
@@ -1,12 +1,16 @@
 package com.example.demo.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 
 @Entity
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -13,8 +12,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://10aeb1257aaf-8360.pro604cr.amypo.ai/proxy/9001")
+                        new Server().url("https://9279.pro604cr.amypo.ai/")
                 ));
-    }
+        }
 }
