4dcbd114223cac0cee67c61f8287587ef49d9a3e
bala-amypo <bala@amypo.in>
2025-12-18 07:48:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ShipmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ShipmentServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.demo.service.impl;
 
 import com.example.demo.entity.Shipment;
 import com.example.demo.entity.Vehicle;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.ShipmentRepository;
 import com.example.demo.repository.VehicleRepository;
 import com.example.demo.service.ShipmentService;
